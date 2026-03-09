c5f3e324c9fd7c28646b1b3fda13d94438f039d7
Leylin Benitez <131553730+leylinbenitez@users.noreply.github.com>
2026-03-08 23:18:45 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,4 @@
 // Client
-console.log("My address:", pg.wallet.publicKey.toString());
+console.log("Dirección del cliente:", pg.wallet.publicKey.toString());
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+console.log(`Saldo disponible: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
